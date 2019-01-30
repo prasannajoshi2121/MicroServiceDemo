7972d6ead1cc9850db0a137b95db3796d1b1a6a0
prasannajoshi2121 <prasannajoshi2121@gmail.com>
2019-01-30 14:03:09 +0530

    Update Application.java

--- src/main/java/com/demo/microservice/Application.java
+++ src/main/java/com/demo/microservice/Application.java
@@ -3,6 +3,9 @@ package com.demo.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+Test
+*/
 @SpringBootApplication
 public class Application 
 {
